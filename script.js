cb3ae0f00f0e43f71b049a2132082152f7865655
taniaafzal110 <38942967+taniaafzal110@users.noreply.github.com>
2018-10-08 17:50:59 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function initialiser() {
+	function alphabet(a) {
+		document.querySelector(".card-header").innnerHTML += a ;
+		console.log(a);
+	}
+}
+function spaceLetter() {
+	document.querySelector(".card-header").innnerHTML += "";
+	console.log("");
+
+}
+function enter() {
+	result = "your message has been sent!"
+	alert(result);
+
+}
